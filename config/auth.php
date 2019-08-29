97e4c2da74029bb40903efb9fceee1f1d334a862
Jason Varga <jason@pixelfear.com>
2019-08-29 14:04:00 -0400

    Use statamic auth driver

--- config/auth.php
+++ config/auth.php
@@ -67,14 +67,8 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',
-            'model' => App\User::class,
+            'driver' => 'statamic',
         ],
-
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
     ],
 
     /*
