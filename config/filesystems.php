1cd7c8cd2b445fdd53a83022086a70ec4df65980
Jack McDade <jack@jackmcdade.com>
2019-10-07 14:47:05 -0400

    :art: Assets disk and container

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,13 @@ return [
             'visibility' => 'public',
         ],
 
+        'assets' => [
+            'driver' => 'local',
+            'root' => public_path('img'),
+            'url' => '/img',
+            'visibility' => 'public',
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
