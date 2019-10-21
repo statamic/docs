4b9a01495aa145e436914e0003ae79e336ba2f3d
Jack McDade <jack@jackmcdade.com>
2019-10-21 10:13:47 -0400

    :art: Add a space

--- resources/js/app.js
+++ resources/js/app.js
@@ -36,16 +36,16 @@ require('./anchors.js')
 require('./external-links.js')
 
 let phrases = [
-    "🍳What's shakin', home skillet?",
-    "🐢Cowabunga!",
-    "🥤Who loves orange soda?",
-    "🔩I put the screw in the tuna!",
-    "🤪I know you are but what am I?",
-    "💣You da bomb!",
-    "🤚Talk to the hand!",
-    "🔥Let's get crunk!",
-    "✂️Cut. It. Out.",
-    "💥These docs are all that and a bag of chips.",
+    "🍳 What's shakin', home skillet?",
+    "🐢 Cowabunga!",
+    "🥤 Who loves orange soda?",
+    "🔩 I put the screw in the tuna!",
+    "🤪 I know you are but what am I?",
+    "💣 You da bomb!",
+    "🤚 Talk to the hand!",
+    "🔥 Let's get crunk!",
+    "✂️ Cut. It. Out.",
+    "💥 These docs are all that and a bag of chips.",
 ]
 
 console.log(phrases[Math.floor(Math.random() * phrases.length)])
