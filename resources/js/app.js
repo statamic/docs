db15fc1fde03b28490cab77f4693312727ff1cb0
Jack McDade <jack@jackmcdade.com>
2020-04-20 15:50:44 -0400

    :pencil: Clean up app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,35 +1,3 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
-// require('./bootstrap');
-
-// window.Vue = require('vue');
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-// const app = new Vue({
-//     el: '#statamic',
-// });
-
-
 require('./prism.js')
 require('./scrollspy.js')
 // require('./nav.js')
@@ -37,7 +5,7 @@ require('./anchors.js')
 require('./external-links.js')
 
 let phrases = [
-    "🍳 What's shakin', home skillet?",
+    "🍳 What's cookin', home skillet?",
     "🐢 Cowabunga!",
     "🥤 Who loves orange soda?",
     "🔩 I put the screw in the tuna!",
