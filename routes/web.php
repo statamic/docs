704d28b74ae74900aa17eb0a119d1ce7e1d51a1e
Jack McDade <jack@jackmcdade.com>
2021-03-25 12:10:16 -0400

    Set a meta title for the KB

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ Route::statamic('/', 'home', ['load' => '/documentation']);
 
 Route::statamic('search-results', 'search', ['hide_sidebar' => true]);
 Route::redirect('screencasts', '/screencasts/installation');
-Route::statamic('knowledge-base', 'knowledge-base.index');
+Route::statamic('knowledge-base', 'knowledge-base.index', ['title' => 'Knowledge Base']);
 Route::permanentRedirect('collections-and-entries', 'collections');
 Route::permanentRedirect('entries', 'collections#entries');
 
