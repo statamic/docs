eb31f62c4b23e56f0fdeb90e5d860d76488e215e
Jack McDade <jack@jackmcdade.com>
2021-07-27 09:06:33 -0400

    Fix installing link and add redirect

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,6 @@ Route::statamic('search-results', 'search', ['hide_sidebar' => true]);
 Route::redirect('screencasts', '/screencasts/installation');
 Route::permanentRedirect('collections-and-entries', 'collections');
 Route::permanentRedirect('entries', 'collections#entries');
+Route::permanentRedirect('installation', 'installing');
 
 Route::redirect('fieldtypes/partial', '/blueprints#importing-fieldsets');
