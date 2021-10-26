4b8d2f029b996b668ca9af8d0045088a79e10302
Jack McDade <jack@jackmcdade.com>
2021-10-26 12:24:27 -0400

    change repositories URL pattern

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ Route::statamic('search-results', 'search', ['hide_sidebar' => true]);
 Route::statamic('sitemap.xml', 'sitemap', ['content_type' => 'xml', 'layout' => 'sitemap']);
 
 // Redirects
+
 Route::permanentRedirect('collections-and-entries', 'collections');
 Route::permanentRedirect('template-engines', 'blade-templates');
 Route::permanentRedirect('entries', 'collections#entries');
@@ -16,4 +17,6 @@ Route::permanentRedirect('fieldtypes/partial', '/blueprints#importing-fieldsets'
 Route::permanentRedirect('cascade', 'data-inheritance');
 Route::permanentRedirect('content-api', 'rest-api');
 Route::permanentRedirect('using-front-end-frameworks', 'javascript-frameworks');
+Route::permanentRedirect('/content-queries/{slug}', '/repositories/{slug}');
+Route::permanentRedirect('repositories', 'content-queries');
 // Route::permanentRedirect('extending/queries', 'content-queries');
