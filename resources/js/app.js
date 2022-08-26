545994bb23dd8a74ab743533e46c19cbf6b8e192
Jack McDade <jack@jackmcdade.com>
2022-08-26 16:15:59 -0400

    New Algolia Infrastructure

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,9 +16,9 @@ window.dayjs = dayjs;
 
 docsearch({
   container: '#docsearch',
-  appId: 'BH4D9OD16A',
+  appId: '90UJMUR5MX',
   indexName: 'statamic_3',
-  apiKey: 'b5e8f73c7462a6d5c8b525ef183aabec',
+  apiKey: '2cea01a83bd805b6c642d3bda0b91437',
   transformItems(items) {
     return items.map((item) => {
         // Transform the absolute URL into a relative URL so it works locally.
