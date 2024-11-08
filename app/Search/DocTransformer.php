a668760f8a11f91e7c947a0084c9b4d4bdf1a38d
John Koster <john@stillat.com>
2024-11-07 19:33:16 -0600

    Add another example

--- app/Search/DocTransformer.php
+++ app/Search/DocTransformer.php
@@ -10,9 +10,13 @@ class DocTransformer implements DocumentTransformer
 {
     public function handle(DocumentFragment $fragment, $entry): void
     {
-        // Add some extra details to the "additional_context".
+        // Add some extra details to "additional_context"
         if (Str::containsAll($fragment->content, ['clear', 'cache'])) {
             $fragment->additionalContextData[] = 'delete cache';
+        }
+
+        if (Str::contains($fragment->content, 'JS Drivers')) {
+            $fragment->additionalContextData[] = 'javascript drivers';
         }
     }
 }
