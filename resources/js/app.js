600d87be763fd4224f4128d5e47f307dfd20749f
Jack McDade <jack@jackmcdade.com>
2024-11-12 14:58:39 -0500

    No port of entry 🚢

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,7 +15,7 @@ window.dayjs = dayjs;
 
 docsearch({
     container: "#docsearch",
-    host: "https://search.statamic.dev:7700",
+    host: "https://search.statamic.dev",
     apiKey: "a8b8f82076221f9595dceca971be29c36cbccd772de5dbdb7f43dfac41557f95",
     indexUid: "default",
     hotKeys: ['ctrl+k', '/']
