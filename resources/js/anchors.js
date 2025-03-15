0763247ae32804c52d2f3dcdc1c0cd82cd0d29c1
Jay George <contact@jaygeorge.co.uk>
2025-03-15 23:44:50 +0000

    Fix anchors.js

--- resources/js/anchors.js
+++ resources/js/anchors.js
@@ -1,7 +1,12 @@
 // Add header anchors
 var elements = document.querySelectorAll('article h2, article h3, article h4');
 Array.prototype.forEach.call(elements, function (el, i) {
-    if (el.id) {
-        el.innerHTML += '<a href="#' + el.id + '" class="anchor">#</a>';
+    // Generate an ID if one doesn't exist
+    if (!el.id) {
+        // Create ID from heading text: lowercase, remove special chars, replace spaces with hyphens
+        el.id = el.textContent.toLowerCase()
+            .replace(/[^\w\s-]/g, '')
+            .replace(/\s+/g, '-');
     }
+    el.innerHTML += '<a href="#' + el.id + '" class="anchor">#</a>';
 });
