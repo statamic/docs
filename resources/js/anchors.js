8a4210448d719136827a0931a674d84597e5cab9
Jay George <contact@jaygeorge.co.uk>
2025-03-18 17:26:27 +0000

    Toc work -- somewhat working

--- resources/js/anchors.js
+++ resources/js/anchors.js
@@ -1,5 +1,5 @@
 // Add header anchors
-var elements = document.querySelectorAll('article h2, article h3, article h4');
+var elements = document.querySelectorAll('article h2, article h3');
 Array.prototype.forEach.call(elements, function (el, i) {
     // Generate an ID if one doesn't exist
     if (!el.id) {
@@ -8,5 +8,8 @@ Array.prototype.forEach.call(elements, function (el, i) {
             .replace(/[^\w\s-]/g, '')
             .replace(/\s+/g, '-');
     }
+    // Add scroll spy timeline track class and incrementing style
+    el.classList.add('o-scroll-spy-timeline__track');
+    el.setAttribute('style', '--ti-name: --' + (i + 1));
     el.innerHTML += '<a href="#' + el.id + '" class="anchor">#</a>';
 });
