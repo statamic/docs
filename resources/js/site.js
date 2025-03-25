b60061c44a86f4d1932988871c77bd5ad5e55f32
Jay George <contact@jaygeorge.co.uk>
2025-03-25 18:09:33 +0000

    Pills

--- resources/js/site.js
+++ resources/js/site.js
@@ -5,6 +5,7 @@ import './anchors.js';
 import './cookies.js';
 import './color-scheme-preferences.js';
 import './external-links.js';
+import './dl.js';
 import './language-badges.js';
 import './searchHotKeys.js';
 import './dayjs.js';
