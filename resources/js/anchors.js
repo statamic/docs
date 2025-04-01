0339bcf96f97a601385231d517dde6177434faa8
Jay George <contact@jaygeorge.co.uk>
2025-04-01 12:02:38 +0100

    Toc/anchors -- fix condition where emojis at the end of headings would break anchors

--- resources/js/anchors.js
+++ resources/js/anchors.js
@@ -12,7 +12,8 @@ Array.prototype.forEach.call(elements, function (el, i) {
         // Create ID from heading text: lowercase, remove special chars, replace spaces with hyphens
         el.id = el.textContent.toLowerCase()
             .replace(/[^\w\s-]/g, '')
-            .replace(/\s+/g, '-');
+            .replace(/\s+/g, '-')
+            .replace(/-+$/, ''); // Remove trailing hyphens which can be cause when an emoji is added to the end e.g. "Writing pure Antlers in Blade 🆕"
     }
     // Add scroll spy timeline track class and incrementing style
     el.classList.add('o-scroll-spy-timeline__track');
