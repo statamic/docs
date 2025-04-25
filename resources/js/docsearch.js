0e39b3abc843275629517dae98154b575a7de00b
Jay George <contact@jaygeorge.co.uk>
2025-04-25 11:19:03 +0100

    Scroll to current menu item if we came from a docsearch -- progress

--- resources/js/docsearch.js
+++ resources/js/docsearch.js
@@ -9,10 +9,22 @@ let hotKeys = [
     "/",
 ];
 
+// Set flag when docsearch button is clicked
+document.addEventListener('click', (e) => {
+    if (e.target.closest('#docsearch button')) {
+        console.log('Setting scrollNextPage from button click');
+        sessionStorage.setItem('scrollNextPage', 'true');
+    }
+});
+
 docsearch({
     container: "#docsearch",
     host: "https://search.statamic.dev",
     apiKey: "a8b8f82076221f9595dceca971be29c36cbccd772de5dbdb7f43dfac41557f95",
     indexUid: "default",
     hotKeys: hotKeys,
+    onOpen: () => {
+        console.log('Setting scrollNextPage from onOpen');
+        sessionStorage.setItem('scrollNextPage', 'true');
+    }
 });
