df242e6190dc982067fdaedae19706641764c12c
Jay George <contact@jaygeorge.co.uk>
2025-07-08 19:53:59 +0100

    Collapsible menus -- Progress with collapse state / turn it into a function

--- resources/js/collapsible-side-menu.js
+++ resources/js/collapsible-side-menu.js
@@ -1,30 +1,33 @@
 // Initialize collapse-nav to 'no' if it hasn't been set yet
 if (!localStorage.getItem('collapse-nav')) {
     localStorage.setItem('collapse-nav', 'no');
-    console.log('not set yet');
+}
+
+// Function to handle navigation collapse/expand
+function toggleNavigation() {
+    // Toggle the collapse-nav state between 'yes' and 'no'
+    const isCurrentlyCollapsed = localStorage.getItem('collapse-nav');
+    const newCollapseState = isCurrentlyCollapsed === 'no' ? 'yes' : 'no';
+    localStorage.setItem('collapse-nav', newCollapseState);
+    
+    if (localStorage.getItem('collapse-nav') === 'yes') {
+        document.querySelectorAll('.o-toggle-subnav').forEach(element => {
+            if (!element.matches(':has(+ ul .o-current-menu-item)')) {
+                element.querySelector('input').checked = false;
+            }
+        });
+        console.log('not collapsed');
+    } else {
+        document.querySelectorAll('.o-toggle-subnav').forEach(element => {
+            if (!element.matches(':has(+ ul .o-current-menu-item)')) {
+                element.querySelector('input').checked = true;
+            }
+        });
+        console.log('collapsed');
+    }
 }
 
 Array.from(document.querySelectorAll('.js__expand-collapse-nav') || []).forEach(element => {
-    element.onclick = function(){
-        // Toggle the collapse-nav state between 'yes' and 'no'
-        const isCurrentlyCollapsed = localStorage.getItem('collapse-nav');
-        const newCollapseState = isCurrentlyCollapsed === 'no' ? 'yes' : 'no';
-        localStorage.setItem('collapse-nav', newCollapseState);
-        if (localStorage.getItem('collapse-nav') === 'yes') {
-            document.querySelectorAll('.o-toggle-subnav').forEach(element => {
-                if (!element.matches(':has(+ ul .o-current-menu-item)')) {
-                    element.querySelector('input').checked = false;
-                }
-            });
-            console.log('not collapsed');
-        } else {
-            document.querySelectorAll('.o-toggle-subnav').forEach(element => {
-                if (!element.matches(':has(+ ul .o-current-menu-item)')) {
-                    element.querySelector('input').checked = true;
-                }
-            });
-            console.log('collapsed');
-        }
-    }
+    element.onclick = toggleNavigation;
 });
 
