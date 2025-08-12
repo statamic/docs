8cb624caf01213278f4b05e866fdfa0ddfe25b7f
Duncan McClean <duncan@duncanmcclean.com>
2025-08-12 11:18:03 +0100

    Add config options

---
+++ config/docs.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    'version' => env('STATAMIC_DOCS_VERSION'),
+
+    'versions' => [
+        [
+            'version' => '5.0',
+            'url' => 'https://statamic.dev',
+        ],
+        [
+            'version' => '6.0',
+            'url' => 'https://v6.statamic.dev',
+        ],
+    ],
+
+];
