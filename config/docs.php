ac7cb164401f4c8359879d68f8d3b1d79e2d2e49
Duncan McClean <duncan@duncanmcclean.com>
2025-08-14 14:49:34 +0100

    Determine the branch based on current version

--- config/docs.php
+++ config/docs.php
@@ -2,15 +2,17 @@
 
 return [
 
-    'version' => env('STATAMIC_DOCS_VERSION'),
+    'version' => env('STATAMIC_DOCS_VERSION', '5.0'),
 
     'versions' => [
         [
             'version' => '6.0',
+            'branch' => '6.x',
             'url' => 'https://v6.statamic.dev',
         ],
         [
             'version' => '5.0',
+            'branch' => '5.x',
             'url' => 'https://statamic.dev',
         ],
     ],
