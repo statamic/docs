d7587bf442974bbe8a924a1646d1e964fc71984a
Jason Varga <jason@pixelfear.com>
2025-08-21 13:12:54 -0400

    use plain number for version

--- config/docs.php
+++ config/docs.php
@@ -2,16 +2,16 @@
 
 return [
 
-    'version' => env('STATAMIC_DOCS_VERSION', '5.0'),
+    'version' => env('STATAMIC_DOCS_VERSION', '5'),
 
     'versions' => [
         [
-            'version' => '6.0',
+            'version' => '6',
             'branch' => '6.x',
             'url' => 'https://v6.statamic.dev',
         ],
         [
-            'version' => '5.0',
+            'version' => '5',
             'branch' => '5.x',
             'url' => 'https://statamic.dev',
         ],
