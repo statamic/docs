2e0687503d3b7b3ec89a32aeb3156a42552966a6
Jason Varga <jason@pixelfear.com>
2025-09-02 16:27:32 -0400

    spacing

--- resources/views/partials/component-example.blade.php
+++ resources/views/partials/component-example.blade.php
@@ -1,5 +1,5 @@
 {!! Statamic\Facades\Markdown::parse('```html'.PHP_EOL.$code.PHP_EOL.'```') !!}
 
-<div class="border px-12 py-6 flex justify-center">
+<div class="border px-12 py-6 flex justify-center mb-[var(--spacing-m)]">
     {!! $code !!}
 </div>
