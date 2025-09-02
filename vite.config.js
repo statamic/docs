fac063adf1f84e4c0ff2f8f336db0f3a6d659e72
Jack McDade <jack@jackmcdade.com>
2025-09-02 12:43:23 -0400

    refactor vite workflow, get tw compiling

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,26 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue2 from '@vitejs/plugin-vue2';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         laravel({
             input: [
                 // CSS
-                'public/css/style.css',
+                "resources/css/style.css",
                 // JS
-                'resources/js/site.js',
+                "resources/js/site.js",
             ],
         }),
-        vue2(),
     ],
+    publicDir: "public",
+    server: {
+        fs: {
+            allow: [".."],
+        },
+    },
+    css: {
+        devSourcemap: true,
+    },
 });
