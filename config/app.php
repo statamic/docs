2f51fe075a1f2ad23de65d6a9859eb2d8a5f955a
Shift <shift@laravelshift.com>
2025-09-12 11:02:14 +0000

    Re-register service providers

--- config/app.php
+++ config/app.php
@@ -6,23 +6,5 @@ return [
 
     'timezone' => 'America/New_York',
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Laravel Framework Service Providers...
-         */
-
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-    ])->toArray(),
 
 ];
