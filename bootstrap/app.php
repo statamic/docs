faf141e1de2549f418b9e36090f1424f8af38b10
Duncan McClean <duncan@duncanmcclean.com>
2025-09-12 12:09:55 +0100

    Don't need the HOME constant in our AppServiceProvider

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,10 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->redirectGuestsTo(fn () => route('login'));
-        $middleware->redirectUsersTo(AppServiceProvider::HOME);
-
-        $middleware->throttleApi();
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
