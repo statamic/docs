ea78ef7f7580c231493ea5b0780a6305221c2875
Jason Varga <jason@pixelfear.com>
2026-03-02 09:36:42 -0500

    composer update and config allowlist

--- config/statamic/system.php
+++ config/statamic/system.php
@@ -128,4 +128,17 @@ return [
     'ajax_timeout' => '600000',
     'pcre_backtrack_limit' => '-1',
 
+    /*
+    |--------------------------------------------------------------------------
+    | View Config Allowlist
+    |--------------------------------------------------------------------------
+    |
+    | Config keys that are allowed to be accessed in Antlers templates. Use
+    | '@default' to include Statamic's default list. Add 'docs' so the docs
+    | version switcher can access config.docs.version and config.docs.versions.
+    |
+    */
+
+    'view_config_allowlist' => ['@default', 'docs'],
+
 ];
