db9a56fb9bd50eb6a4e96e4447a8aca58ae44900
Graham Reynolds <insidelane@gmail.com>
2026-06-14 22:01:31 +1000

    The recent-updates page was missing from v6 docs

--- routes/redirects.php
+++ routes/redirects.php
@@ -149,7 +149,7 @@ Route::permanentRedirect('deploying/ploi', '/getting-started/deploying/ploi');
 Route::permanentRedirect('preferences', '/control-panel/preferences');
 Route::permanentRedirect('protecting-content', '/frontend/protecting-content');
 Route::permanentRedirect('quick-start-guide', '/getting-started/quick-start-guide');
-Route::permanentRedirect('recent-updates', '/');
+Route::permanentRedirect('recent-updates', '/knowledge-base/recent-updates');
 Route::permanentRedirect('relationships', '/content-modeling/relationships');
 Route::permanentRedirect('release-schedule-support-policy', '/knowledge-base/release-schedule-support-policy');
 Route::permanentRedirect('requirements', '/getting-started/requirements');
